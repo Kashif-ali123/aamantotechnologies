288d561b819bd8e126dab91f2e748dd0db0e2103
Kashif-ali123 <89835659+Kashif-ali123@users.noreply.github.com>
2021-10-09 02:10:02 -0700

    Add files via upload

--- src/components/Common/Blog1Banner.js
+++ src/components/Common/Blog1Banner.js
@@ -1,12 +1,12 @@
 import React from 'react'
  import ReactWOW from 'react-wow'
 import {Link} from 'gatsby'
-import bannerImg from '../../assets/images/banner1-img.jpg'
+import bannerImg from '../../assets/images/banner-img11.png'
 // import shape1 from '../../assets/images/shape/shape1.svg'
 // import shape2 from '../../assets/images/shape/shape2.png'
 // import shape3 from '../../assets/images/shape/shape3.png'
 
-const ProjectBanner = () => {
+const Blog1Banner = () => {
     return (
         <div className="page-title-area">
         <div className="container">
@@ -14,12 +14,14 @@ const ProjectBanner = () => {
                 <div className="col-lg-6 col-md-12">
                     <div className="main-banner-content">
                         <ReactWOW delay='.1s' animation='fadeInLeft'>
-                            <h1 style={{fontSize:"30px"}}>Project Management</h1>
+                            <h1 style={{fontSize:"30px"}}>How AI Chatbot Technology is Changing The Health Industry  </h1>
                         </ReactWOW>
 
                         <ReactWOW delay='.1s' animation='fadeInLeft'>
-                            <p style={{textAlign:"justify"}}>Agile project management offers a host of potential benefits to the IT organization, including faster deployments, greater adaptability, and better alignment with business or customer needs. The adoption of agile mindsets and frameworks is a proven success in todays delivery teams.</p>
-                           
+                            <p style={{textAlign:"justify"}}>There is no denying that artificial intelligence is having a significant impact on the healthcare industry. According to Accenture research, more than 40% of healthcare executives believe AI will have the most critical influence on their organizations within the next three years. In addition, various types of artificial intelligence, such as predictive analytics or machine learning, are already being used by healthcare practitioners to address multiple concerns. </p>
+                            {/* <p style={{textAlign:"justify"}}>But do you think cloud adoption is easy?  </p>
+                            <p style={{textAlign:"justify"}}>If you're curious to learn more about cloud adoption, you've come to the right place. As we will discuss the many stages of cloud adoption in this article, we will better understand how to proceed with cloud migration and the various procedures that occur. </p>
+                            <p style={{textAlign:"justify"}}>Let's have a peek at the Cloud journey before moving on.</p> */}
                         </ReactWOW>
 
                         <ReactWOW delay='.1s' animation='fadeInRight'>
@@ -36,7 +38,7 @@ const ProjectBanner = () => {
                 <div className="col-lg-6 col-md-12">
                     <ReactWOW delay='.1s' animation='fadeInUp'>
                         <div className="main-banner-layer">
-                            <img src={bannerImg} alt="banner" />
+                            <img src={bannerImg} alt="banner" style={{borderRadius:"100px 50px 100px 50px", height:"360px", marginBottom:"100px"}} />
                         </div>
                     </ReactWOW>
                 </div>
@@ -48,4 +50,4 @@ const ProjectBanner = () => {
     )
 }
 
-export default ProjectBanner;
+export default Blog1Banner;
--- src/components/Common/BlogBanner.js
+++ src/components/Common/BlogBanner.js
@@ -1,12 +1,12 @@
 import React from 'react'
  import ReactWOW from 'react-wow'
 import {Link} from 'gatsby'
-import bannerImg from '../../assets/images/banner1-img.jpg'
+import bannerImg from '../../assets/images/banner-img10.png'
 // import shape1 from '../../assets/images/shape/shape1.svg'
 // import shape2 from '../../assets/images/shape/shape2.png'
 // import shape3 from '../../assets/images/shape/shape3.png'
 
-const ProjectBanner = () => {
+const BlogBanner = () => {
     return (
         <div className="page-title-area">
         <div className="container">
@@ -14,12 +14,14 @@ const ProjectBanner = () => {
                 <div className="col-lg-6 col-md-12">
                     <div className="main-banner-content">
                         <ReactWOW delay='.1s' animation='fadeInLeft'>
-                            <h1 style={{fontSize:"30px"}}>Project Management</h1>
+                            <h1 style={{fontSize:"30px"}}>Stages of Cloud Adoption </h1>
                         </ReactWOW>
 
                         <ReactWOW delay='.1s' animation='fadeInLeft'>
-                            <p style={{textAlign:"justify"}}>Agile project management offers a host of potential benefits to the IT organization, including faster deployments, greater adaptability, and better alignment with business or customer needs. The adoption of agile mindsets and frameworks is a proven success in todays delivery teams.</p>
-                           
+                            <p style={{textAlign:"justify"}}>Cloud is the future; in the coming time, everything will be handled within the cloud systems, and we won't have to worry about storage, encryption, or any other such factor. </p>
+                            {/* <p style={{textAlign:"justify"}}>But do you think cloud adoption is easy?  </p>
+                            <p style={{textAlign:"justify"}}>If you're curious to learn more about cloud adoption, you've come to the right place. As we will discuss the many stages of cloud adoption in this article, we will better understand how to proceed with cloud migration and the various procedures that occur. </p>
+                            <p style={{textAlign:"justify"}}>Let's have a peek at the Cloud journey before moving on.</p> */}
                         </ReactWOW>
 
                         <ReactWOW delay='.1s' animation='fadeInRight'>
@@ -36,7 +38,7 @@ const ProjectBanner = () => {
                 <div className="col-lg-6 col-md-12">
                     <ReactWOW delay='.1s' animation='fadeInUp'>
                         <div className="main-banner-layer">
-                            <img src={bannerImg} alt="banner" />
+                            <img src={bannerImg} alt="banner" style={{borderRadius:"100px 50px 100px 50px"}} />
                         </div>
                     </ReactWOW>
                 </div>
@@ -48,4 +50,4 @@ const ProjectBanner = () => {
     )
 }
 
-export default ProjectBanner;
+export default BlogBanner;
