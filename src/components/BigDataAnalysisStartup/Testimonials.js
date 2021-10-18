dc2abd39d4ce80346c5b7344c632f5e63170b82d
Kashif-ali123 <89835659+Kashif-ali123@users.noreply.github.com>
2021-10-17 23:29:37 -0700

    Add files via upload

--- src/components/BigDataAnalysisStartup/Testimonials.js
+++ src/components/BigDataAnalysisStartup/Testimonials.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import user1 from '../../assets/images/user1.jpg'
-import user2 from '../../assets/images/user2.jpg'
-import user3 from '../../assets/images/user3.jpg'
+import user1 from '../../assets/images/services/gather.png'
+import user2 from '../../assets/images/services/featured.jpg'
+import user3 from '../../assets/images/services/images.png'
+import user4 from '../../assets/images/services/strategy.png'
+import user5 from '../../assets/images/services/stock.png'
+import user6 from '../../assets/images/services/download.png'
 import shape9 from '../../assets/images/shape/vector-shape9.png'
 import shape10 from '../../assets/images/shape/vector-shape10.png'
 import Loadable from '@loadable/component'
@@ -42,48 +45,87 @@ const Testimonials = () => {
         <section className="testimonials-area bg-23173a pt-100 pb-70">
             <div className="container">
                 <div className="section-title">
-                    <h2>What Our Clients are Saying?</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                    <h2>PROCESS OF CUSTOM SOFTWARE DEVELOPMENT</h2>
+                    <p>Aamanto web design and development team has developed a number of useful services to help you transform your business.</p>
                 </div>
 
                 {display ? <OwlCarousel 
                     className="testimonials-slides owl-carousel owl-theme"
                     {...options}
                 > 
-                    <div className="single-testimonials-box">
-                        <img src={user1} className="shadow-sm" alt="testimonials" />
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
-
+                    <div className="single-testimonials-box" style={{height:"454px"}}>
+                        <img src={user1} className="shadow-sm" alt="testimonials" style={{backgroundColor:"#fff", width:"300px"}} />
+                        <h1 style={{marginTop:"100px"}}>Gathering Information</h1>
+                        <p>Clients are our top priority, and we endeavor to provide them with only the best service. As a result, we begin by examining the expectations and requirements of our clients. Our team gathers all relevant information about your company, goals, market, rivals, and other factors before moving forward.</p>
+{/* 
                         <div className="client-info">
                             <div className="title">
                                 <h3>Alex Maxwell</h3>
                                 <span>CEO at EnvyTheme</span>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
 
-                    <div className="single-testimonials-box">
+                    <div className="single-testimonials-box" style={{height:"454px"}}>
                         <img src={user2} className="shadow-sm" alt="testimonials" />
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+                        <h1 style={{marginTop:"100px"}}>Features Declarations</h1>
+                        <p>We will produce a framework detailing those elements, including features, design, and functions, once we have all of the crucial facts that our client is seeking on the website. Clients are able to review the pre-defined guidelines and make any adjustments they see appropriate.</p>
 
-                        <div className="client-info">
+                        {/* <div className="client-info">
                             <div className="title">
                                 <h3>David Warner</h3>
                                 <span>CEO at Envato</span>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
 
-                    <div className="single-testimonials-box">
+                    <div className="single-testimonials-box" style={{height:"454px"}}>
                         <img src={user3} className="shadow-sm" alt="testimonials" />
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+                        <h1 style={{marginTop:"100px"}}>Strategy Formulation</h1>
+                        <p>Our team moves forward with the strategy development process after enumerating all of the demands and comprehending our clients' perspectives. Our experts will offer the ideal solution for your project based on your objectives, ensuring that you receive only the best web development services.</p>
 
-                        <div className="client-info">
+                        {/* <div className="client-info">
                             <div className="title">
                                 <h3>Sarah Taylor</h3>
                                 <span>CEO at ThemeForest</span>
                             </div>
-                        </div>
+                        </div> */}
+                    </div>
+                    <div className="single-testimonials-box">
+                        <img src={user4} className="shadow-sm" alt="testimonials" />
+                        <h1 style={{marginTop:"100px"}}>Design and Coding</h1>
+                        <p>Following that, we'll begin the coding and design process while keeping the standards insight. Our in-house designer will take care of all the essential details for a perfect and user-friendly website. They'll also make sure the website adheres to all of Google's and SEO's most recent rules. All modifications will be sent to the clients for finalization.</p>
+
+                        {/* <div className="client-info">
+                            <div className="title">
+                                <h3>Sarah Taylor</h3>
+                                <span>CEO at ThemeForest</span>
+                            </div>
+                        </div> */}
+                    </div>
+                    <div className="single-testimonials-box">
+                        <img src={user5} className="shadow-sm" alt="testimonials" />
+                        <h1 style={{marginTop:"100px"}}>Debugging</h1>
+                        <p>We are pleased with our high-quality service and will not compromise it at any cost. We've established a separate team of skilled workers to oversee the tasks we do. The website will be sent to the customer once it has been approved. We also provide our clients with post-delivery support to guarantee that they fully comprehend the functionality.</p>
+
+                        {/* <div className="client-info">
+                            <div className="title">
+                                <h3>Sarah Taylor</h3>
+                                <span>CEO at ThemeForest</span>
+                            </div>
+                        </div> */}
+                    </div>
+                    <div className="single-testimonials-box">
+                        <img src={user6} className="shadow-sm" alt="testimonials" />
+                        <h1 style={{marginTop:"100px"}}>Project Execution</h1>
+                        <p>Websites are prone to bugs and technological faults, as we all know. As a result, each designed website must go through a rigorous quality testing process before being delivered. The client has access to it after it is clear of any problems and issues. We also keep in touch with the client on a regular basis in case he or she has any concerns about the website.</p>
+
+                        {/* <div className="client-info">
+                            <div className="title">
+                                <h3>Sarah Taylor</h3>
+                                <span>CEO at ThemeForest</span>
+                            </div>
+                        </div> */}
                     </div>
                 </OwlCarousel> : ''}
             </div>
